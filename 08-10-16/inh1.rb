47c6d775da36997064c48e823f37962ab6801547
venkatesh <venkatesh.sv645@gmail.com>
2016-10-10 07:44:23 +0530

    Rails project started

---
+++ 08-10-16/inh1.rb
@@ -0,0 +1,73 @@
+# # class Dog
+# # 	def initialize(breed)
+# # 		@breed=breed
+# # end
+# # end
+
+# # class Lab < Dog
+# # 	def initialize(breed,name)
+# # 		@name=name
+# # 		super(breed)
+		
+# # 	end
+
+
+# # 	# def to_s
+# # 	# 	puts "#{@name},#{@breed}"
+		
+# # 	# end
+# # end
+
+# # a=Lab.new("Labrader","tommy")
+# # puts a.to_s
+
+
+
+# class A  
+ 
+# end  
+  
+# class B < A  
+
+# 	 def a(name)
+	 
+#     puts "In class A #{name}"
+#   end 
+
+
+#   def a  (x1,x2)
+
+#     puts "In class B #{x1},#{x2}"  
+#   end  
+
+# end  
+  
+# b = B.new  
+# b.a("a")
+
+
+class Bicycle  
+  # attr_reader :gears, :wheels, :seats  
+  def a
+    puts @wheels
+  end  
+end  
+  
+class Tandem < Bicycle  
+  def initialize(gears,wheels,seats)  
+    # super  
+    # @seat	s = 2  
+     @wheels = wheels 
+    @seats = seats
+    @gears = gears 
+  end  
+end  
+t = Tandem.new(2,2,3)  
+t.a
+# puts t.gears
+# puts t.wheels  
+# puts t.seats  
+# b = Bicycle.new  
+# puts b.gears  
+# puts b.wheels  
+# puts b.seats  
